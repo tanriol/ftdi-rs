1ddbc5bef5a4b8e01a6b5085b8eeaee33199ccd4
Denis Lisov <dennis.lissov@gmail.com>
2016-02-02 18:59:33 +0300

    initial commit

---
+++ examples/dlp-loopback-tester.rs
@@ -0,0 +1,62 @@
+//! A simple test script for testing the loopback features
+//! of the FTDI DLP-HS-FPGA3's default FPGA firmware.
+//! Mainly used as a compile test
+
+extern crate ftdi;
+
+use std::io::{Read, Write};
+
+
+fn main() {
+    println!("Starting tester...");
+    let mut context = ftdi::Context::new();
+    context.set_interface(ftdi::Interface::A).unwrap();
+
+    if context.usb_open(0x0403, 0x6010).is_ok() {
+        println!("Device found and opened");
+        context.usb_reset().unwrap();
+        context.usb_purge_buffers().unwrap();
+        context.set_latency_timer(2).unwrap();
+
+        // Junk test
+        let mut junk = vec![];
+        context.read_to_end(&mut junk).unwrap();
+        if junk.len() > 0 {
+            println!("Junk in line: {:?}", junk);
+        }
+
+        // Ping test
+        context.write_all(&vec![0x00]).unwrap();
+        let mut reply = vec![];
+        context.read_to_end(&mut reply).unwrap();
+        if reply != vec![0x56] {
+            println!("Wrong ping reply {:?} (expected {:?}", reply, vec![0x56]);
+        }
+
+        for num in 0u16..256 {
+            let num = num as u8;
+
+            // Loopback test
+            context.write_all(&vec![0x20, num]).unwrap();
+            let mut reply = vec![];
+            context.read_to_end(&mut reply).unwrap();
+            if reply != vec![num] {
+                println!("Wrong loopback reply {:?} (expected {:?}", reply, vec![num]);
+            }
+
+            // Complement loopback test
+            context.write_all(&vec![0x21, num]).unwrap();
+            let mut reply = vec![];
+            context.read_to_end(&mut reply).unwrap();
+            let complement = 255 - num;
+            if reply != vec![complement] {
+                println!("Wrong complement reply {:?} (expected {:?}",
+                         reply,
+                         vec![complement]);
+            }
+        }
+        println!("Testing finished");
+    } else {
+        println!("Cannot find/open device, runtime tests are NOP");
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+//! An incomplete Rust wrapper over the `libftdi1` library for working with FTDI devices
+//!
+//! Note: the library interface is *definitely* unstable for now
+
+extern crate num;
+
+extern crate libftdi1_sys as ffi;
+
+use std::io;
+use std::io::{Read, Write, ErrorKind};
+
+use num::traits::ToPrimitive;
+
+
+/// The target interface
+pub enum Interface {
+    A,
+    B,
+    C,
+    D,
+    Any,
+}
+
+impl Into<ffi::ftdi_interface> for Interface {
+    fn into(self) -> ffi::ftdi_interface {
+        match self {
+            Interface::A => ffi::ftdi_interface::INTERFACE_A,
+            Interface::B => ffi::ftdi_interface::INTERFACE_B,
+            Interface::C => ffi::ftdi_interface::INTERFACE_C,
+            Interface::D => ffi::ftdi_interface::INTERFACE_D,
+            Interface::Any => ffi::ftdi_interface::INTERFACE_ANY,
+        }
+    }
+}
+
+
+pub struct Context {
+    native: ffi::ftdi_context,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        let mut context = Context { native: Default::default() };
+        let result = unsafe { ffi::ftdi_init(&mut context.native) };
+        // Can be non-zero on either OOM or libusb_init failure
+        assert!(result == 0);
+        context
+    }
+
+    /// Do not call after opening the USB device
+    pub fn set_interface(&mut self, interface: Interface) -> io::Result<()> {
+        let result = unsafe { ffi::ftdi_set_interface(&mut self.native, interface.into()) };
+        match result {
+            0 => Ok(()),
+            -1 => Err(io::Error::new(ErrorKind::InvalidInput, "unknown interface")),
+            -2 => Err(io::Error::new(ErrorKind::NotFound, "device not found")),
+            -3 => Err(io::Error::new(ErrorKind::Other, "device already opened")),
+            _ => Err(io::Error::new(ErrorKind::Other, "unknown set latency error")),
+        }
+    }
+
+    pub fn usb_open(&mut self, vendor: u16, product: u16) -> io::Result<()> {
+        let result = unsafe { ffi::ftdi_usb_open(&mut self.native, vendor as i32, product as i32) };
+        match result {
+            0 => Ok(()),
+            -3 => Err(io::Error::new(ErrorKind::NotFound, "device not found")),
+            -4 => Err(io::Error::new(ErrorKind::Other, "unable to open device")),
+            -5 => Err(io::Error::new(ErrorKind::Other, "unable to claim device")),
+            -6 => Err(io::Error::new(ErrorKind::Other, "reset failed")),
+            -7 => Err(io::Error::new(ErrorKind::Other, "set baudrate failed")),
+            -8 => Err(io::Error::new(ErrorKind::Other, "get description failed")),
+            -9 => Err(io::Error::new(ErrorKind::Other, "get serial failed")),
+            -12 => Err(io::Error::new(ErrorKind::Other, "libusb_get_device_list failed")),
+            -13 => Err(io::Error::new(ErrorKind::Other, "libusb_get_device_descriptor failed")),
+            _ => Err(io::Error::new(ErrorKind::Other, "unknown usb_open error")),
+        }
+    }
+
+    pub fn usb_reset(&mut self) -> io::Result<()> {
+        let result = unsafe { ffi::ftdi_usb_reset(&mut self.native) };
+        match result {
+            0 => Ok(()),
+            -1 => Err(io::Error::new(ErrorKind::Other, "reset failed")),
+            -2 => Err(io::Error::new(ErrorKind::NotFound, "device not found")),
+            _ => Err(io::Error::new(ErrorKind::Other, "unknown reset error")),
+        }
+    }
+
+    pub fn usb_purge_buffers(&mut self) -> io::Result<()> {
+        let result = unsafe { ffi::ftdi_usb_purge_buffers(&mut self.native) };
+        match result {
+            0 => Ok(()),
+            -1 => Err(io::Error::new(ErrorKind::Other, "read purge failed")),
+            -2 => Err(io::Error::new(ErrorKind::Other, "write purge failed")),
+            -3 => Err(io::Error::new(ErrorKind::NotFound, "device not found")),
+            _ => Err(io::Error::new(ErrorKind::Other, "unknown purge error")),
+        }
+    }
+
+    pub fn set_latency_timer(&mut self, value: u8) -> io::Result<()> {
+        let result = unsafe { ffi::ftdi_set_latency_timer(&mut self.native, value) };
+        match result {
+            0 => Ok(()),
+            -1 => Err(io::Error::new(ErrorKind::InvalidInput, "bad latency value")),
+            -2 => Err(io::Error::new(ErrorKind::Other, "set latency failed")),
+            -3 => Err(io::Error::new(ErrorKind::NotFound, "device not found")),
+            _ => Err(io::Error::new(ErrorKind::Other, "unknown set latency error")),
+        }
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        unsafe { ffi::ftdi_deinit(&mut self.native) }
+    }
+}
+
+impl Read for Context {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = buf.len().to_i32().unwrap_or(std::i32::MAX);
+        let result = unsafe { ffi::ftdi_read_data(&mut self.native, buf.as_mut_ptr(), len) };
+        match result {
+            count if count >= 0 => Ok(count as usize),
+            -666 => Err(io::Error::new(ErrorKind::NotFound, "device not found in read")),
+            libusb_error => {
+                Err(io::Error::new(ErrorKind::Other,
+                                   format!("libusb_bulk_transfer error {}", libusb_error)))
+            }
+        }
+    }
+}
+
+impl Write for Context {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let len = buf.len().to_i32().unwrap_or(std::i32::MAX);
+        let result = unsafe { ffi::ftdi_write_data(&mut self.native, buf.as_ptr(), len) };
+        match result {
+            count if count >= 0 => Ok(count as usize),
+            -666 => Err(io::Error::new(ErrorKind::NotFound, "device not found in write")),
+            libusb_error => {
+                Err(io::Error::new(ErrorKind::Other,
+                                   format!("usb_bulk_write error {}", libusb_error)))
+            }
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn it_works() {}
+}
